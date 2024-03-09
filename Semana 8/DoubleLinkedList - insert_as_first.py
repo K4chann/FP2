4f28875be3d663126af828857ab64abad89edd89
K4chann <124625996+K4chann@users.noreply.github.com>
2024-03-09 18:52:39 +0000

    Update and rename DoubleLinkedList - insert_as_first to DoubleLinkedList - insert_as_first.py

--- Semana 8/DoubleLinkedList - insert_as_first.py
+++ Semana 8/DoubleLinkedList - insert_as_first.py
@@ -33,6 +33,7 @@ class DoubleLinkedList:
     # Escriba su código debajo de esta línea
     def insert_as_first(self, value):
         """Método que inserta en una lista doblemente encadenada un valor."""
+    
         if self.__first is None:
             self.__first = self.Node(value)
             self.__last = self.__first
@@ -40,4 +41,13 @@ class DoubleLinkedList:
             prev = self.__first
             self.__first = self.Node(value, None, prev)
             prev.prev_node = self.__first
+        
         self.__len += 1
+
+if __name__ == "__main__":
+    l = DoubleLinkedList();
+    for num in range(10, -1, -1):
+        l.insert_as_first(num)
+
+    for num in l:
+        print(num)
