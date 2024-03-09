448ca420b1bb02b27561763964d09d2117acd5b6
K4chann <124625996+K4chann@users.noreply.github.com>
2024-03-09 20:01:11 +0000

    Update and rename Java - zip to Java - zip.java

--- Semana XI/Java - zip.java
+++ Semana XI/Java - zip.java
@@ -5,7 +5,8 @@ public class ArrayTools {
         int[][] result = new int[Math.min(array1.length, array2.length)][2];
 	    
 	    for (int row = 0; row < result.length; row++) {
-	        result[row][0] = array1[row]; result[row][1] = array2[row];
+	        result[row][0] = array1[row];
+            result[row][1] = array2[row];
 	    }
 	    
 	    return result;
