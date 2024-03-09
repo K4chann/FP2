d92b62edfbef04cb80a96dd20dd120b8bb9681fe
K4chann <124625996+K4chann@users.noreply.github.com>
2024-03-09 20:14:38 +0000

    Update Java - swap.java

--- Semana XII/Java - swap.java
+++ Semana XII/Java - swap.java
@@ -4,9 +4,14 @@ public class Functions {
 	public static String swap (String text) {
 	    String splittedText[] = text.split(" ");
 	    String resultText = "";
+
+        // Al igual que se explicó en otro ejercicio, es màs eficiente usar StringBuilder.
+        // Pero no se volverá a repetir aquí, pueden mirar el ejercicio de la semana XI (Concatenar array)
 	    
-	    for (String word: splittedText) {
-	        String initial = ((Character) word.charAt(0)).toString().toUpperCase();
+	    for (String word : splittedText) {
+	        String initial = (
+                (Character) word.charAt(0)
+            ).toString().toUpperCase();
 	        resultText = initial + word.substring(1).toLowerCase() + " " + resultText;
 	    }
 	    return resultText.trim();
